89037025d8e7a103018dde90a6a4c3e03aa59f07
Kevin Ullyott <kevin.ullyott@canyongbs.com>
2026-05-11 21:02:07 -0400

    Create plugin and set it up in the Service Provider
    Signed-off-by: Kevin Ullyott <kevin.ullyott@canyongbs.com>

--- src/Support/ModularLivewireServiceProvider.php
+++ src/Support/ModularLivewireServiceProvider.php
@@ -1,67 +1,17 @@
 <?php
 
-namespace Glhd\ModularLivewire\Support;
+namespace InterNACHI\ModularLivewire\Support;
 
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use InterNACHI\Modular\PluginRegistry;
+use InterNACHI\ModularLivewire\ModularLivewirePlugin;
 
 class ModularLivewireServiceProvider extends ServiceProvider
 {
-	public function boot()
+	public function register(): void
 	{
-		// require_once __DIR__.'/helpers.php';
-		// $this->bootConfig();
-		// $this->bootViews();
-		// $this->bootBladeComponents();
-	}
-	
-	public function register()
-	{
-		$this->mergeConfigFrom($this->packageConfigFile(), 'modular-livewire');
-	}
-	
-	protected function bootViews() : self
-	{
-		$this->loadViewsFrom($this->packageViewsDirectory(), 'modular-livewire');
+		$this->app->singleton(ModularLivewirePlugin::class);
 		
-		$this->publishes([
-			$this->packageViewsDirectory() => $this->app->resourcePath('views/vendor/modular-livewire'),
-		], 'modular-livewire-views');
-		
-		return $this;
-	}
-	
-	protected function bootBladeComponents() : self
-	{
-		if (version_compare($this->app->version(), '8.0.0', '>=')) {
-			Blade::componentNamespace('Glhd\\ModularLivewire\\Components', 'modular-livewire');
-		}
-		
-		return $this;
-	}
-	
-	protected function bootConfig() : self
-	{
-		$this->publishes([
-			$this->packageConfigFile() => $this->app->configPath('modular-livewire.php'),
-		], 'modular-livewire-config');
-		
-		return $this;
-	}
-	
-	protected function packageConfigFile(): string
-	{
-		return dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'modular-livewire.php';
-	}
-	
-	protected function packageTranslationsDirectory(): string
-	{
-		return dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'translations';
-	}
-	
-	protected function packageViewsDirectory(): string
-	{
-		return dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'views';
+		PluginRegistry::register(ModularLivewirePlugin::class);
 	}
 }
