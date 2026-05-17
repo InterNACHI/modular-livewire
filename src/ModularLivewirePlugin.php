e117401896a411182ee9be88d44a61f1d02843dc
Stuart Pomeroy <stuart@pomeroys.site>
2026-05-17 13:50:07 +0100

    Improve registration of livewire components to properly use the correct APIs

--- src/ModularLivewirePlugin.php
+++ src/ModularLivewirePlugin.php
@@ -19,45 +19,43 @@ class ModularLivewirePlugin extends Plugin
 		protected ModuleRegistry $registry,
 	) {
 	}
-	
+
 	public static function boot(Closure $handler, Application $app): void
 	{
 		if (class_exists(Livewire::class)) {
 			$handler(static::class);
 		}
 	}
-	
+
 	/**
 	 * @return array<int, array{module: string, name: string, fqcn: string}>
 	 */
 	public function discover(FinderFactory $finders): iterable
 	{
-		return FinderCollection::forFiles()
-			->name('*.php')
-			->inOrEmpty($this->registry->getModulesPath().'/*/src/Livewire')
-			->withModuleInfo()
-			->values()
-			->map(fn(ModuleFileInfo $file) => [
-				'module' => $file->module()->name,
-				'name' => Str::of($file->getRelativePath())
-					->explode('/')
-					->filter()
-					->push($file->getBasename('.php'))
-					->map(fn(string $segment) => Str::kebab($segment))
-					->implode('.'),
-				'fqcn' => $file->fullyQualifiedClassName(),
-			])
-			->toArray();
+        return FinderCollection::forDirectories()
+            ->name('Livewire')
+            ->inOrEmpty($this->registry->getModulesPath().'/*/src')
+            ->withModuleInfo()
+            ->values()
+            ->map(fn(ModuleFileInfo $file) => [
+                'prefix' => $file->module()->name,
+                'namespace' => $file->module()->qualify('Livewire'),
+                'viewFolder' => $file->module()->path('resources/views/livewire')
+            ])->toArray();
 	}
-	
+
 	/**
 	 * @param Collection<int, array{module: string, name: string, fqcn: string}> $data
 	 */
 	public function handle(Collection $data): void
 	{
-		$data->each(fn(array $row) => Livewire::component(
-			"{$row['module']}::{$row['name']}",
-			$row['fqcn'],
-		));
+        $data->each(function(array $row) {
+            Livewire::addLocation($row['viewFolder']);
+            Livewire::addNamespace($row['prefix'], $row['viewFolder']);
+            Livewire::addLocation(classNamespace: $row['namespace']);
+            Livewire::addNamespace(namespace: $row['prefix'],
+                classNamespace: $row['namespace'],
+                classViewPath: $row['viewFolder']);
+        });
 	}
 }
